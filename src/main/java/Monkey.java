1d26f1c7094f7ef53376647581bd3d9123022858
l.floresm <l.floresm@alumnos.upm.es>
2020-11-15 20:34:14 +0100

    Add monkey functionality (not working yet)

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,19 +1,108 @@
 import java.util.Random;
+import java.util.concurrent.Semaphore;
 
 public class Monkey implements Runnable {
 
     private int GET_ROPE_TIME = 1000;
     private int GET_TRAVEL_TIME = 4000;
+    private int MAX_MONKEYS_ON_ROPE = 5;
     private String side;
     private int monkeyId;
 
-    public Monkey(String side, int monkeyId) {
-        this.side = side;
+    static Semaphore mutex = new Semaphore(1, true);
+    static Semaphore eastQueue = new Semaphore(0, true);
+    static Semaphore westQueue = new Semaphore(0, true);
+    static String ropeSide = "East";
+    static int westMonkeysWaiting = 0;
+    static int eastMonkeysWaiting = 0;
+    //static int monkeysOnRope = 0;
+    static int westMonkeysOnRope = 0;
+    static int eastMonkeysOnRope = 0;
+
+    public Monkey(int monkeyId) {
+        this.side = getInitialSide();
         this.monkeyId = monkeyId;
     }
 
     public void run() {
+        System.out.println("Hello, it's monkey " + this.monkeyId +
+                " and my side is " + this.side);
 
+        if(this.side == "East") {
+            travelWest();
+        } else {
+            travelEast();
+        }
+    }
+
+    private void travelWest() {
+        try {
+            //Thread.sleep(getSpawnTime());
+            System.out.println("Hello, it's monkey " + this.monkeyId +
+                " and i want to cross");
+
+            mutex.acquire();
+            while(westMonkeysOnRope > 0) {
+                eastMonkeysWaiting++;
+                mutex.release();
+                System.out.println("Me enfado y no respiro " + this.monkeyId);
+                eastQueue.acquire();
+                mutex.acquire();
+            }
+
+            if(eastMonkeysOnRope < MAX_MONKEYS_ON_ROPE) {
+                Thread.sleep(GET_ROPE_TIME);
+                eastMonkeysOnRope++;
+                System.out.println("Hello, it's monkey " + this.monkeyId +
+                        " and i'm crossing");
+                mutex.release();
+                mutex.acquire();
+                eastMonkeysOnRope--;
+                System.out.println("Hello, it's monkey " + this.monkeyId +
+                        " and i've crossed");
+            }
+            mutex.release();
+
+            //Thread.sleep(GET_TRAVEL_TIME);
+
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+    private void travelEast() {
+        try {
+            //Thread.sleep(getSpawnTime());
+            System.out.println("Hello, it's monkey " + this.monkeyId +
+                    " and i want to cross");
+
+            mutex.acquire();
+            while(eastMonkeysOnRope > 0) {
+                westMonkeysWaiting++;
+                mutex.release();
+                System.out.println("Me enfado y no respiro " + this.monkeyId);
+                westQueue.acquire();
+                mutex.acquire();
+            }
+
+            if(westMonkeysOnRope < MAX_MONKEYS_ON_ROPE) {
+                Thread.sleep(GET_ROPE_TIME);
+                westMonkeysOnRope++;
+                System.out.println("Hello, it's monkey " + this.monkeyId +
+                        " and i'm crossing");
+                mutex.release();
+                mutex.acquire();
+                westMonkeysOnRope--;
+                System.out.println("Hello, it's monkey " + this.monkeyId +
+                        " and i've crossed");
+            }
+            mutex.release();
+
+            //Thread.sleep(GET_TRAVEL_TIME);
+
+        } catch (InterruptedException e) {
+            System.err.println(e.getMessage());
+        }
     }
 
     private String getInitialSide() {
@@ -38,6 +127,9 @@ public class Monkey implements Runnable {
     }
 
     public static void main(String[] args) {
-        
+        int totalMonkeys = 5;
+        for (int i = 0; i < totalMonkeys; i++) {
+            new Thread(new Monkey(i)).start();
+        }
     }
 }
