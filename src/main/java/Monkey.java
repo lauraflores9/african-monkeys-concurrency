e8f70c25cb03e00b37eac774f100d51bba528cf0
l.floresm <l.floresm@alumnos.upm.es>
2020-11-15 18:22:28 +0100

    Add random side generator

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,9 +1,20 @@
 import java.util.Random;
 
 public class Monkey implements Runnable {
-    
+
     public void run() {
 
+    }
+
+    private String getInitialSide() {
+        final int MIN_LIMIT = 1;
+        final int MAX_LIMIT = 10;
+        final int SIDE_THRESHOLD = 5;
+
+        Random ran = new Random();
+        int side = ran.nextInt((MAX_LIMIT - MIN_LIMIT) + 1) + MIN_LIMIT;
+
+        return side <= SIDE_THRESHOLD ? "East" : "West";
     }
 
     public static void main(String[] args) {
