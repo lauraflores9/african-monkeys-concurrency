6fcf3e44c1f9a526b0de9f33d801359833d80e7a
l.floresm <l.floresm@alumnos.upm.es>
2020-11-16 18:57:05 +0100

    Clean duplicate code

--- src/main/java/Monkey.java
+++ src/main/java/Monkey.java
@@ -1,142 +1,101 @@
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
+enum Direction {
+    WEST,
+    EAST;
+
+    public static Direction getRandomDirection() {
+        return values()[(int) (Math.random() * values().length)];
+    }
+
+    public static Direction getOpposite(Direction dir) {
+        switch(dir) {
+            case WEST: return EAST;
+            case EAST: return WEST;
+            default: throw new IllegalStateException();
+        }
+    }
+}
+
 public class Monkey implements Runnable {
 
-    private int GET_ROPE_TIME = 1000;
-    private int GET_TRAVEL_TIME = 4000;
-    private int MAX_MONKEYS_ON_ROPE = 1;
-    private String side;
+    private final int ROPE_TIME = 1000;
+    private final int TRAVEL_TIME = 4000;
+    final int MIN_ARRIVAL_TIME = 1000;
+    final int MAX_ARRIVAL_TIME = 8000;
+
+    private Direction side;
     private int monkeyId;
 
     static Semaphore mutex = new Semaphore(1, true);
-    static Semaphore eastQueue = new Semaphore(0, true);
-    static Semaphore westQueue = new Semaphore(0, true);
-    static String ropeSide = "East";
-    static int westMonkeysWaiting = 0;
-    static int eastMonkeysWaiting = 0;
-    //static int monkeysOnRope = 0;
-    static int westMonkeysOnRope = 0;
-    static int eastMonkeysOnRope = 0;
+    static Semaphore queue[] = {new Semaphore(0, true), new Semaphore(0, true)};
+    static int waiting[] = {0, 0};
+    static int onRope[] = {0, 0};
 
     public Monkey(int monkeyId) {
-        this.side = getInitialSide();
+        this.side = Direction.getRandomDirection();
         this.monkeyId = monkeyId;
     }
 
     public void run() {
-        System.out.println("Hello, it's monkey " + this.monkeyId +
-                " and my side is " + this.side);
         try {
-            int sleepTime = getSpawnTime();
-            System.out.println("Info: " + this.monkeyId + " " + sleepTime);
-            Thread.sleep(getSpawnTime());
+            int sleepTime = getArrivalTime();
+            System.out.println("Monkey: " + this.monkeyId +
+                    " from " + this.side +
+                    " ArrivalWait: " + sleepTime);
+            Thread.sleep(sleepTime);
+            travel();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        if(this.side == "East") {
-            travelWest();
-        } else {
-            travelEast();
-        }
     }
 
-    private void travelWest() {
+    private void travel() {
         try {
-            //Thread.sleep(getSpawnTime());
-            System.out.println("Hello, it's monkey " + this.monkeyId +
-                " and i want to cross");
-
+            int direction = this.side.ordinal();
+            int oppositeDirection = Direction.getOpposite(this.side).ordinal();
+            System.out.println("Monkey: " + this.monkeyId +
+                    " from " + this.side +
+                    " wants to cross");
             mutex.acquire();
-            while(westMonkeysOnRope > 0) {
-                eastMonkeysWaiting++;
-                System.out.println("Me enfado y no respiro " + this.monkeyId);
+            while(onRope[oppositeDirection] > 0) {
+                waiting[direction]++;
+                System.out.println("Monkey: " + this.monkeyId +
+                        " from " + this.side +
+                        " is waiting");
                 mutex.release();
-                eastQueue.acquire();
+                queue[direction].acquire();
                 mutex.acquire();
             }
-
-            Thread.sleep(GET_ROPE_TIME);
-            eastMonkeysOnRope++;
-            System.out.println("Hello, it's monkey " + this.monkeyId +
-                    " and i'm crossing, we are " + eastMonkeysOnRope +
-                    " bros on the rope");
+            Thread.sleep(ROPE_TIME);
+            onRope[direction]++;
+            System.out.println("Monkey: " + this.monkeyId +
+                    " from " + this.side +
+                    " is crossing");
             mutex.release();
-            Thread.sleep(GET_TRAVEL_TIME);
+            Thread.sleep(TRAVEL_TIME);
             mutex.acquire();
-            eastMonkeysOnRope--;
-            System.out.println("Hello, it's monkey " + this.monkeyId +
-                    " and i've crossed");
-            if(eastMonkeysOnRope == 0) {
-                while(westMonkeysWaiting-- > 0) {
-                    westQueue.release();
+            onRope[direction]--;
+            System.out.println("Monkey: " + this.monkeyId +
+                    " from " + this.side +
+                    " has crossed");
+            if(onRope[direction] == 0) {
+                while(waiting[oppositeDirection]-- > 0) {
+                    queue[oppositeDirection].release();
                 }
             }
             mutex.release();
-
         } catch (InterruptedException e) {
             System.err.println(e.getMessage());
         }
     }
 
-    private void travelEast() {
-        try {
-            //Thread.sleep(getSpawnTime());
-            System.out.println("Hello, it's monkey " + this.monkeyId +
-                    " and i want to cross");
-
-            mutex.acquire();
-            while(eastMonkeysOnRope > 0) {
-                westMonkeysWaiting++;
-                System.out.println("Me enfado y no respiro " + this.monkeyId);
-                mutex.release();
-                westQueue.acquire();
-                mutex.acquire();
-            }
-
-            Thread.sleep(GET_ROPE_TIME);
-            westMonkeysOnRope++;
-            System.out.println("Hello, it's monkey " + this.monkeyId +
-                    " and i'm crossing, we are " + westMonkeysOnRope +
-                    " bros on the rope");
-            mutex.release();
-            Thread.sleep(GET_TRAVEL_TIME);
-            mutex.acquire();
-            westMonkeysOnRope--;
-            System.out.println("Hello, it's monkey " + this.monkeyId +
-                    " and i've crossed");
-            if(westMonkeysOnRope == 0) {
-                while(eastMonkeysWaiting-- > 0) {
-                    eastQueue.release();
-                }
-            }
-            mutex.release();
-
-        } catch (InterruptedException e) {
-            System.err.println(e.getMessage());
-        }
-    }
-
-    private String getInitialSide() {
-        final int MIN_LIMIT = 1;
-        final int MAX_LIMIT = 10;
-        final int SIDE_THRESHOLD = 5;
-
+    private int getArrivalTime() {
         Random ran = new Random();
-        int initialSide = ran.nextInt((MAX_LIMIT - MIN_LIMIT) + 1) + MIN_LIMIT;
+        int time = ran.nextInt((MAX_ARRIVAL_TIME - MIN_ARRIVAL_TIME) + 1) + MIN_ARRIVAL_TIME;
 
-        return initialSide <= SIDE_THRESHOLD ? "East" : "West";
-    }
-
-    private int getSpawnTime() {
-        final int MIN_TIME = 1;
-        final int MAX_TIME = 8;
-
-        Random ran = new Random();
-        int time = ran.nextInt((MAX_TIME - MIN_TIME) + 1) + MIN_TIME;
-
-        return time * 1000;
+        return time;
     }
 
     public static void main(String[] args) {
